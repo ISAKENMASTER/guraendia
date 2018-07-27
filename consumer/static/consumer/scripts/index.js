902d3a9dd988d82bfb4c521ab9e6fbe634939f20
isaken <kmwangi9398@gmail.com>
2018-07-27 16:42:16 +0300

    added corausel for right lower items

---
+++ consumer/static/consumer/scripts/index.js
@@ -0,0 +1,8 @@
+$('.carosel-control-right').click(function() {
+  $(this).blur();
+  $(this).parent().find('.carosel-item').first().insertAfter($(this).parent().find('.carosel-item').last());
+});
+$('.carosel-control-left').click(function() {
+  $(this).blur();
+  $(this).parent().find('.carosel-item').last().insertBefore($(this).parent().find('.carosel-item').first());
+});
