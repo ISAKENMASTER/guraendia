f2411a5d5d64c8d60eac787316feb2fff92b0bf8
isaken <kmwangi9398@gmail.com>
2018-07-30 21:32:05 +0300

    minor changes

--- consumer/static/consumer/scripts/index.js
+++ consumer/static/consumer/scripts/index.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 $('.carosel-control-right').click(function() {
   $(this).blur();
   $(this).parent().find('.carosel-item').first().insertAfter($(this).parent().find('.carosel-item').last());
@@ -5,4 +6,22 @@ $('.carosel-control-right').click(function() {
 $('.carosel-control-left').click(function() {
   $(this).blur();
   $(this).parent().find('.carosel-item').last().insertBefore($(this).parent().find('.carosel-item').first());
+});
+
+
+$('.itDescBtn').click(function () {
+$('.createDesc').toggle();
+
+    });
+
+$(window).scroll(function() {
+    if($(window).scrollTop() == $(document).height() - $(window).height()) {
+        appendData();
+    }
+});
+function appendData() {
+     var orginal = $('#sI');
+     orginal.clone().css({}).appendTo('#sPace');
+        }
+
 });
